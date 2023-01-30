cb7ac85919ab1cd59eac0e8f8b199656113e89e8
Steve Bruner <sbruner@slipfire.com>
2023-01-29 22:19:35 -0500

    all subnav in right column to scroll

--- components/TableOfContents.tsx
+++ components/TableOfContents.tsx
@@ -45,6 +45,7 @@ export function TableOfContents({toc}) {
             margin-bottom: 1rem;
             padding: 0.5rem 0 0;
             border-left: 1px solid var(--border-color);
+            overflow-y: auto;
           }
           ul {
             margin: 0;
